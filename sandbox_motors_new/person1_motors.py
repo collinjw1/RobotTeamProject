6ff089812b5aa86ccd258d6fcbe04423e515a326
collinjw1 <collinjw@rose-hulman.edu>
2018-05-02 21:21:13 -0400

    Done

--- sandbox_motors_new/person1_motors.py
+++ sandbox_motors_new/person1_motors.py
@@ -28,6 +28,7 @@ def test_forward_backward():
       3. Same as #2, but runs forward_by_encoders.
       4. Same as #1, 2, 3, but tests the BACKWARD functions.
     """
+    print()
     print('** Testing of forward_seconds is starting **')
     print()
 
@@ -46,6 +47,106 @@ def test_forward_backward():
             stop_action = ev3.Motor.STOP_ACTION_HOLD
 
         forward_seconds(seconds, speed, stop_action)
+
+    print()
+    print('** Testing of forward_by_time is starting **')
+    print()
+
+    while True:
+        inches = int(input('Enter a distance for the robot to travel: '))
+        if inches == 0:
+            break
+        speed = int(input('Enter a speed for the robot to travel at between 0 and 100: '))
+        stop_action = str(input('Enter a stop action (brake, coast, or hold): '))
+
+        if stop_action == 'brake':
+            stop_action = ev3.Motor.STOP_ACTION_BRAKE
+        elif stop_action == 'coast':
+            stop_action = ev3.Motor.STOP_ACTION_COAST
+        else:
+            stop_action = ev3.Motor.STOP_ACTION_HOLD
+
+        forward_by_time(inches, speed, stop_action)
+
+    print()
+    print('** Testing of forward_by_encoders is starting **')
+    print()
+
+    while True:
+        inches = int(input('Enter a distance for the robot to travel: '))
+        if inches == 0:
+            break
+        speed = int(input('Enter a speed for the robot to travel at between 0 and 100: '))
+        stop_action = str(input('Enter a stop action (brake, coast, or hold): '))
+
+        if stop_action == 'brake':
+            stop_action = ev3.Motor.STOP_ACTION_BRAKE
+        elif stop_action == 'coast':
+            stop_action = ev3.Motor.STOP_ACTION_COAST
+        else:
+            stop_action = ev3.Motor.STOP_ACTION_HOLD
+
+        forward_by_encoders(inches, speed, stop_action)
+
+    print()
+    print('** Testing of backward_seconds is starting **')
+    print()
+    while True:
+        seconds = int(input('Enter a number of seconds to travel for: '))
+        if seconds == 0:
+            break
+        speed = int(input('Enter a speed for the robot to travel at between 0 and 100: '))
+        stop_action = str(input('Enter a stop action (brake, coast, or hold): '))
+
+        if stop_action == 'brake':
+            stop_action = ev3.Motor.STOP_ACTION_BRAKE
+        elif stop_action == 'coast':
+            stop_action = ev3.Motor.STOP_ACTION_COAST
+        else:
+            stop_action = ev3.Motor.STOP_ACTION_HOLD
+
+        backward_seconds(seconds, speed, stop_action)
+
+    print()
+    print('** Testing of backward_by_time is starting **')
+    print()
+
+    while True:
+        inches = int(input('Enter a distance for the robot to travel: '))
+        if inches == 0:
+            break
+        speed = int(input('Enter a speed for the robot to travel at between 0 and 100: '))
+        stop_action = str(input('Enter a stop action (brake, coast, or hold): '))
+
+        if stop_action == 'brake':
+            stop_action = ev3.Motor.STOP_ACTION_BRAKE
+        elif stop_action == 'coast':
+            stop_action = ev3.Motor.STOP_ACTION_COAST
+        else:
+            stop_action = ev3.Motor.STOP_ACTION_HOLD
+
+        backward_by_time(inches, speed, stop_action)
+
+    print()
+    print('** Testing of backward_by_encoders is starting **')
+    print()
+
+    while True:
+        inches = int(input('Enter a distance for the robot to travel: '))
+        if inches == 0:
+            break
+        speed = int(input('Enter a speed for the robot to travel at between 0 and 100: '))
+        stop_action = str(input('Enter a stop action (brake, coast, or hold): '))
+
+        if stop_action == 'brake':
+            stop_action = ev3.Motor.STOP_ACTION_BRAKE
+        elif stop_action == 'coast':
+            stop_action = ev3.Motor.STOP_ACTION_COAST
+        else:
+            stop_action = ev3.Motor.STOP_ACTION_HOLD
+
+        backward_by_encoders(inches, speed, stop_action)
+
 
 
 def forward_seconds(seconds, speed, stop_action):
@@ -81,7 +182,7 @@ def forward_by_time(inches, speed, stop_action):
     """
     gogojuice=speed*8
     disd=(inches/(1.3*math.pi))*360
-    time=disd/gogojuice
+    time=disd/(abs(gogojuice))
     forward_seconds(time,speed,stop_action)
 
 
@@ -95,10 +196,18 @@ def forward_by_encoders(inches, speed, stop_action):
     """
     left_motor = ev3.LargeMotor(ev3.OUTPUT_C)
     right_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+
+    assert left_motor.connected
+    assert right_motor.connected
+
     gogojuice = speed * 8
     disd = (inches / (1.3 * math.pi))*360
     left_motor.run_to_rel_pos(position_sp=disd, speed_sp=gogojuice)
-    right_motor.run_to_rel_pos(position_sp=-disd, speed_sp=gogojuice)
+    right_motor.run_to_rel_pos(position_sp=disd, speed_sp=gogojuice)
+    left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+    right_motor.wait_while(ev3.Motor.STATE_RUNNING)
+    left_motor.stop(stop_action=stop_action)
+    right_motor.stop(stop_action=stop_action)
 
 
 def backward_seconds(seconds, speed, stop_action):
@@ -113,7 +222,7 @@ def backward_by_time(inches, speed, stop_action):
 
 def backward_by_encoders(inches, speed, stop_action):
     """ Calls forward_by_encoders with negative speeds to achieve backward motion. """
-    forward_by_encoders(inches,-speed,stop_action)
+    forward_by_encoders(-inches,speed,stop_action)
 
 
 test_forward_backward()
