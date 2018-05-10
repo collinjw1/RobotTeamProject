f36ed8b1204b4c3e8d245c4e99154be84e8069f8
kondraaj <kondraaj@rose-hulman.edu>
2018-05-10 09:09:39 -0400

    all done

--- sandbox/kondraaj/mqtt/m1_pc_shared_circles.py
+++ sandbox/kondraaj/mqtt/m1_pc_shared_circles.py
@@ -104,7 +104,7 @@ def left_mouse_click(event, mqtt_client):
     # Repeated: If you uncommented the code above to test it, make sure to comment it back out before todo7 below.
 
     # MQTT draw
-    # TODO: 7. Send a message using MQTT that will:
+    # DONE: 7. Send a message using MQTT that will:
     #   - Call the method called "on_circle_draw" on the delegate at the other end of the pipe.
     #   - Pass the parameters [my_color, event.x, event.y] as a list.
     # This is the only TO DO you have to think about.  It is meant to help you learn the mqtt_client.send_message syntax
