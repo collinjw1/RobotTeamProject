694c69972cc7b73ef89b8d59d2ecf3defb9fc626
jollydi <jollydi@rose-hulman.edu>
2018-05-23 19:40:29 -0400

    Finished all function definitions

--- projects/jollydi Individual Project/project_pc.py
+++ projects/jollydi Individual Project/project_pc.py
@@ -1,4 +1,9 @@
 import tkinter
 from tkinter import ttk
-
+import pc_delegate as pcd
 import mqtt_remote_method_calls as com
+
+def main():
+    delegate = pcd.Pc_delegate()
+    client = com.MqttClient()
+    client.connect_to_ev3(delegate)
--- sandbox/jollydi/mqtt/m5_pc_remote_drive.py
+++ sandbox/jollydi/mqtt/m5_pc_remote_drive.py
@@ -33,7 +33,7 @@ import mqtt_remote_method_calls as com
 
 def main():
     # DONE: 2. Setup an mqtt_client. Notice that since you don't need to receive any messages you do NOT need to have
-    # a MyDelegate class.  Simply construct the MqttClient with no parameter in the constructor (easy).
+    # a MyDelegate class.  Simply construct the MqttClient with no parameter in the constructor (easy)
 
     mqtt_client = com.MqttClient()
     mqtt_client.connect_to_ev3()
