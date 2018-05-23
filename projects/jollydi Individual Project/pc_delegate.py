79be0beed1ed8dca38262604db58987dfdd89241
jollydi <jollydi@rose-hulman.edu>
2018-05-23 19:33:20 -0400

    Finished all function definitions

--- projects/jollydi Individual Project/pc_delegate.py
+++ projects/jollydi Individual Project/pc_delegate.py
@@ -4,6 +4,10 @@ from tkinter import ttk
 import mqtt_remote_method_calls as com
 
 class Pc_delegate(object):
+    def __init__(self):
+        self.client = com.MqttClient()
+        self.client.connect_to_ev3()
+
     def give_directions(self):
         root = tkinter.Tk()
 
@@ -11,14 +15,18 @@ class Pc_delegate(object):
         main_frame.grid()
 
         left_button = ttk.Button(main_frame, text='Adjust left')
-        left_button['command'] = lambda: send_adjust_left(client)
+        left_button['command'] = lambda: send_adjust_left(self.client)
         left_button.grid(row=0, column=0)
 
         right_button = ttk.Button(main_frame, text='Adjust right')
-        right_button['command'] = lambda: send_adjust_right(client)
+        right_button['command'] = lambda: send_adjust_right(self.client)
         right_button.grid(row=0, column=1)
 
         close_button = ttk.Button(main_frame, text='Close')
         close_button['command'] = lambda: root.destroy()
 
-    def send_adjust_l
+    def send_adjust_left(self, client):
+        client.send_message('adjust_left')
+
+    def send_adjust_right(self, client):
+        client.send_message('adjust_right')
