15c549936d76e65c51cc65f10cf19941bc1fdecf
kondraaj <kondraaj@rose-hulman.edu>
2018-05-24 09:18:56 -0400

    all done

--- projects/kondraaj_IndividualProject_pc.py
+++ projects/kondraaj_IndividualProject_pc.py
@@ -7,14 +7,10 @@ Author: Aaron Kondrat.
 import tkinter
 from tkinter import ttk
 import mqtt_remote_method_calls as com
+import time
 
 
 class BabeRuth(object):
-
-    def __init__(self):
-        self.ball = 0
-        self.strike = 0
-        self.out = 0
 
     def pitch(self):
         root = tkinter.Tk()
@@ -23,29 +19,6 @@ class BabeRuth(object):
         main_frame = ttk.Frame(root, padding=20)
         main_frame.grid()
 
-        # ball_counter = ttk.Label(main_frame, text="Ball")
-        # ball_counter.grid(row=0, column=0)
-        # strike_counter = ttk.Label(main_frame, text="Strike")
-        # strike_counter.grid(row=0, column=1)
-        # out_counter = ttk.Label(main_frame, text="Out")
-        # out_counter.grid(row=0, column=2)
-        #
-        # number_balls = ttk.Label(main_frame, text=self.ball)
-        # number_balls.grid(row=1, column=0)
-        # number_strikes = ttk.Label(main_frame, text=self.strike)
-        # number_strikes.grid(row=1, column=1)
-        # number_outs = ttk.Label(main_frame, text=self.out)
-        # number_outs.grid(row=1, column=2)
-        #
-        # ball = ttk.Button(main_frame, text='Ball')
-        # ball.grid(row=2, column=0)
-        # ball['command'] = lambda: called_ball(self.ball, root, mqtt_client)
-        # strike = ttk.Button(main_frame, text='Strike')
-        # strike.grid(row=2, column=1)
-        # strike['command'] = lambda: called_strike(self.strike, root, mqtt_client)
-        # out = ttk.Button(main_frame, text='Out')
-        # out.grid(row=2, column=2)
-        # out['command'] = lambda: called_out(self.out, root, mqtt_client)
         single = ttk.Button(main_frame, text='Single')
         single.grid(row=3, column=0)
         single['command'] = lambda: run_bases(1, root, mqtt_client)
@@ -70,6 +43,7 @@ mqtt_client.connect_to_ev3()
 def main():
     up_to_bat()
 
+
 def up_to_bat():
     root = tkinter.Tk()
     root.title("Batter Up")
@@ -77,6 +51,8 @@ def up_to_bat():
     main_frame = ttk.Frame(root, padding=20)
     main_frame.grid()
 
+    batter_up = ttk.Label(main_frame, text='Batter Up')
+    batter_up.grid()
     swing = ttk.Button(main_frame, text='Swing')
     swing.grid()
     swing['command'] = lambda: swing_the_bat(root, mqtt_client)
@@ -91,27 +67,6 @@ def run_bases(num, root, client):
 def swing_the_bat(root, client):
     client.send_message('swing_the_bat')
     root.destroy()
-
-
-# def called_ball(ball, root, client):
-#     ball = ball + 1
-#     client.send_message('ball')
-#     root.destroy()
-#     return ball
-#
-#
-# def called_strike(strike, root, client):
-#     strike = strike + 1
-#     client.send_message('strike')
-#     root.destroy()
-#     return strike
-#
-#
-# def called_out(outs, root, client):
-#     outs = outs + 1
-#     client.send_message('out')
-#     root.destroy()
-#     return outs
 
 
 main()
